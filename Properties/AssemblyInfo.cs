acc98089630085114d1d1ade8f5b6b1ecbee78fb
wanderingies <wanderingies@hotmail.com>
2018-02-27 18:53:04 +0800

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("端口映射")]
+[assembly: AssemblyDescription("服务端端口映射表")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("悦澜™")]
+[assembly: AssemblyProduct("端口映射")]
+[assembly: AssemblyCopyright("悦澜 © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9450a634-9115-4dd6-b1d9-832379823d3d")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
